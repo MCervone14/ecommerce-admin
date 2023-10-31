2c2bcf3d7264590c3bf47153cede614d028d5b94
Michael Cervone <MCervone14@gmail.com>
2023-10-31 00:59:17 -0500

    Added sizes, colors, categories admin dashboards

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/CategoriesClient.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { CategoryColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/dataTable";
+
+interface CategoriesClientProps {
+  data: CategoryColumn[];
+}
+
+const CategoriesClient = ({ data }: CategoriesClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+    </>
+  );
+};
+
+export default CategoriesClient;
---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/SizeClient.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { SizeColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/dataTable";
+
+interface SizeClientProps {
+  data: SizeColumn[];
+}
+
+const SizeClient = ({ data }: SizeClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+    </>
+  );
+};
+
+export default SizeClient;
